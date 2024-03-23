9c42da74feccee972864862de9e98e1f820dabb9
ArtursMelmanis <124924639+ArtursMelmanis@users.noreply.github.com>
2024-03-23 16:43:58 +0200

    Update (infinity) main.py

--- main.py
+++ main.py
@@ -10,20 +10,25 @@ def int_input(message: str, number_range: range) -> int:
         except ValueError:
             print('Kļūda, mēģiniet vēlreiz: ', end='')
 
+while True: # Viens no uzdevumiem ir "uzsākt spēli atkārtoti pēc kārtējās spēles pabeigšanas."
+    points = 0
+    bank = 0
+    player = 1
+    number = int_input('Ievadiet skaitli no 20 līdz 30: ', range(20, 31))
+    
+    while number < 3000:
+        print(player, '. spēlētāja gājiens', sep='')
+        number *= int_input('Ievadiet reizinātāju (3, 4 vai 5): ', range(3, 6))
+        points += 1 if number % 2 == 0 else -1
+        bank += 1 if number % 5 == 0 else 0
+        player = (player % 2) + 1
+        print('skailtis:', number, '| punkti:', points, '| banka:', bank)
+    
+    points += bank * (-1 if points % 2 == 0 else 1)
+    print('Spēle beidzās, gala punkti:', points)
+    print('Uzvar ', (points % 2) + 1, '. spēlētājs', sep='')
 
-points = 0
-bank = 0
-player = 1
-number = int_input('Ievadiet skaitli no 20 līdz 30: ', range(20, 31))
-
-while number < 3000:
-    print(player, '. spēlētāja gājiens', sep='')
-    number *= int_input('Ievadiet reizinātāju (3, 4 vai 5): ', range(3, 6))
-    points += 1 if number % 2 == 0 else -1
-    bank += 1 if number % 5 == 0 else 0
-    player = (player % 2) + 1
-    print('skailtis:', number, '| punkti:', points, '| banka:', bank)
-
-points += bank * (-1 if points % 2 == 0 else 1)
-print('Spēle beidzās, gala punkti:', points)
-print('Uzvar ', (points % 2) + 1, '. spēlētājs', sep='')
+    # Cikls būs bezgalīgs, taču mēs varam ļaut spēlētājam pašam izlemt, ko darīt tālak
+    # play_again = input("Vai vēlaties spēlēt vēlreiz? (j/n): ")
+    # if play_again.lower() != "j":
+    #     break
