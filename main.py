9bd12d166bebdc0a6ae8f74c719d29a15fb138f5
MarisZeibe <maris_zeibe@hotmail.com>
2024-03-24 16:58:59 +0200

    Generate and print game tree

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+class State:
+    number: int
+    points: int
+    bank: int
+    level: int
+    children: list
+
+    def __init__(self, number, points, bank, level):
+        self.number = number
+        self.points = points
+        self.bank = bank
+        self.level = level
+        self.children = []
+
+    def __eq__(self, other):
+        if other.__class__ is not self.__class__:
+            return NotImplemented
+        return (self.number, self.points, self.bank, self.level) == (other.number, other.points, other.bank, other.level)
+
+
 def int_input(message: str, number_range: range) -> int:
     print(message, end='')
     while True:
@@ -10,11 +30,43 @@ def int_input(message: str, number_range: range) -> int:
         except ValueError:
             print('Kļūda, mēģiniet vēlreiz: ', end='')
 
-while True: # Viens no uzdevumiem ir "uzsākt spēli atkārtoti pēc kārtējās spēles pabeigšanas."
+
+def generate_tree(index: int):
+    global gameTree
+    if gameTree[index].number > 3000:
+        return
+    for n in range(3, 6):
+        new_state = State(
+            gameTree[index].number * n,
+            gameTree[index].points + (1 if gameTree[index].number % 2 == 0 else -1),
+            gameTree[index].bank + (1 if gameTree[index].number % 5 == 0 else 0),
+            gameTree[index].level + 1
+        )
+        if new_state.number > 3000:
+            new_state.points += bank * (-1 if new_state.points % 2 == 0 else 1)
+        if new_state not in gameTree:
+            gameTree.append(new_state)
+            gameTree[index].children.append(len(gameTree) - 1)
+            generate_tree(len(gameTree) - 1)
+        else:
+            gameTree[index].children.append(gameTree.index(new_state))
+
+
+def print_tree(index=0):
+    print('.' * gameTree[index].level, gameTree[index].number, gameTree[index].points, gameTree[index].bank)
+    for x in gameTree[index].children:
+        print_tree(x)
+
+
+while True:  # Viens no uzdevumiem ir "uzsākt spēli atkārtoti pēc kārtējās spēles pabeigšanas."
     points = 0
     bank = 0
     player = 1
     number = int_input('Ievadiet skaitli no 20 līdz 30: ', range(20, 31))
+
+    gameTree = [State(number, points, bank, 0)]
+    generate_tree(0)
+    print_tree()
     
     while number < 3000:
         print(player, '. spēlētāja gājiens', sep='')
