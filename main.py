d29a69671e3517350a76d439e13d0a675444218a
MarisZeibe <maris_zeibe@hotmail.com>
2024-04-07 13:11:23 +0300

    Improved evaluation function and tree output, fixed tree generation and game end number not being inclusive

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import random
 from enum import Enum
 
+
 class Algorithm(Enum):
     MINIMAX = 1
     ALPHA_BETA = 2
 
+
 class Player(Enum):
     Lietotajs = 1
     Dators = 2
+
 
 def choose_starting_player() -> Player:
     while True:
@@ -21,6 +24,7 @@ def choose_starting_player() -> Player:
         else:
             print("Nepareiza izvēle. Mēģiniet vēlreiz.")
 
+
 def choose_algorithm() -> Algorithm:
     while True:
         print("Izvēlieties algoritmu, kuru izmantos dators:")
@@ -33,52 +37,61 @@ def choose_algorithm() -> Algorithm:
         else:
             print("Nepareiza izvēle. Mēģiniet vēlreiz.")
 
+
 def evaluate_state(state):
-    distance_to_goal = abs(3000 - state.number)
-    points = state.points
-    bank_points = state.bank
-    return distance_to_goal * 0.3 + points * 0.5 + bank_points * 0.2
+    # distance_to_goal = abs(3000 - state.number)
+    # points = state.points
+    # bank_points = state.bank
+    # return distance_to_goal * 0.3 + points * 0.5 + bank_points * 0.2
+    # return 1 if (state.points + ((state.bank * (-1 if state.points % 2 == 0 else 1)) if state.number > 3000 else 0)) % 2 == 0 else -1
+    k = 1 if ((state.bank + state.points) % 2 == 0) else -1
+    x1 = k * min(state.bank, 1)
+    x2 = k * (1 if state.number >= 3000 else 0) * (3000 * 5 - state.number) / (3000 * 5 - 20) #k * (1 if state.number > 3000 else 0) * (15000 - state.number) / (15000 - 20)
+    x3 = k * (0 if state.number >= 3000 else 1) * state.number / (3000 * 5 - 20)
+    return 100*x1 + 10*x2 + 1*x3
+
 
 def minimax_search(state, player):
-  if state.number > 3000:
-      return evaluate_state(state)
+    if state.number >= 3000:
+        return evaluate_state(state)
 
-  if player == Player.Lietotajs:
-      best_value = float('-inf')
-      for child_index in state.children:
-          child_value = minimax_search(gameTree[child_index], Player.Dators)
-          best_value = max(best_value, child_value)
-      return best_value
-  else:
-      best_value = float('inf')
-      for child_index in state.children:
-          child_value = minimax_search(gameTree[child_index], Player.Lietotajs)
-          best_value = min(best_value, child_value)
-      return best_value
+    if player == Player.Lietotajs:
+        best_value = float('-inf')
+        for child_index in state.children:
+            child_value = minimax_search(gameTree[child_index], Player.Dators)
+            best_value = max(best_value, child_value)
+        return best_value
+    else:
+        best_value = float('inf')
+        for child_index in state.children:
+            child_value = minimax_search(gameTree[child_index], Player.Lietotajs)
+            best_value = min(best_value, child_value)
+        return best_value
 
 
 def alpha_beta_search(state, alpha, beta, player):
-  if state.number > 3000:
-      return evaluate_state(state)
+    if state.number >= 3000:
+        return evaluate_state(state)
 
-  if player == Player.Lietotajs:
-      value = float('-inf')
-      for child_index in state.children:
-          child_value = alpha_beta_search(gameTree[child_index], alpha, beta, Player.Dators)
-          value = max(value, child_value)
-          alpha = max(alpha, value)
-          if alpha >= beta:
-              break
-      return value
-  else:
-      value = float('inf')
-      for child_index in state.children:
-          child_value = alpha_beta_search(gameTree[child_index], alpha, beta, Player.Lietotajs)
-          value = min(value, child_value)
-          beta = min(beta, value)
-          if alpha >= beta:
-              break
-      return value
+    if player == Player.Lietotajs:
+        value = float('-inf')
+        for child_index in state.children:
+            child_value = alpha_beta_search(gameTree[child_index], alpha, beta, Player.Dators)
+            value = max(value, child_value)
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return value
+    else:
+        value = float('inf')
+        for child_index in state.children:
+            child_value = alpha_beta_search(gameTree[child_index], alpha, beta, Player.Lietotajs)
+            value = min(value, child_value)
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return value
+
 
 class State:
     number: int
@@ -113,18 +126,18 @@ def int_input(message: str, number_range: range) -> int:
             print('Kļūda, mēģiniet vēlreiz: ', end='')
 
 
-def generate_tree(index: int):
+def generate_tree(index=0):
     global gameTree
     if gameTree[index].number > 3000:
         return
     for n in range(3, 6):
         new_state = State(
             gameTree[index].number * n,
-            gameTree[index].points + (1 if gameTree[index].number % 2 == 0 else -1),
-            gameTree[index].bank + (1 if gameTree[index].number % 5 == 0 else 0),
+            gameTree[index].points + (1 if (gameTree[index].number*n) % 2 == 0 else -1),
+            gameTree[index].bank + (1 if (gameTree[index].number*n) % 5 == 0 else 0),
             gameTree[index].level + 1
         )
-        if new_state.number > 3000:
+        if new_state.number >= 3000:
             new_state.points += bank * (-1 if new_state.points % 2 == 0 else 1)
         if new_state not in gameTree:
             gameTree.append(new_state)
@@ -134,10 +147,18 @@ def generate_tree(index: int):
             gameTree[index].children.append(gameTree.index(new_state))
 
 
-def print_tree(index=0):
-    print('.' * gameTree[index].level, gameTree[index].number, gameTree[index].points, gameTree[index].bank)
-    for x in gameTree[index].children:
-        print_tree(x)
+def print_tree(generate_values=False, index=0):
+    print('\t' * gameTree[index].level, end='')
+    print(f'number: {gameTree[index].number:<5}', end=' ')
+    print(f'points: {gameTree[index].points:<2}', end=' ')
+    print(f'bank: {gameTree[index].bank}', end=' ')
+    if generate_values:
+        print(f'value: {evaluate_state(gameTree[index]):<9.4f}', end=' ')
+        current_player = player.Lietotajs if gameTree[index].level % 2 == 0 else player.Dators
+        print(f'minimax: {minimax_search(gameTree[index], current_player):.4f}', end='')
+    print()
+    for state_index in gameTree[index].children:
+        print_tree(generate_values, state_index)
 
 
 while True:
@@ -148,10 +169,10 @@ while True:
     number = int_input('Ievadiet skaitli no 20 līdz 30: ', range(20, 31))
 
     gameTree = [State(number, points, bank, 0)]
-    generate_tree(0)
-    print_tree()
-    
-    while number < 3000:
+    generate_tree()
+    print_tree(True)
+
+    while number <= 3000:
         print(player, '. spēlētāja gājiens', sep='')
         if player == Player.Lietotajs:
             number *= int_input('Ievadiet reizinātāju (3, 4 vai 5): ', range(3, 6))
@@ -190,13 +211,13 @@ while True:
 
                 number *= best_move
                 print('Datora gājiens. Izvēlēts skaitlis:', best_move)
-        
+
         points += 1 if number % 2 == 0 else -1
         bank += 1 if number % 5 == 0 else 0
-        #player = (player % 2) + 1
+        # player = (player % 2) + 1
         player = Player.Lietotajs if player == Player.Dators else Player.Dators
         print('skailtis:', number, '| punkti:', points, '| banka:', bank)
-    
+
     points += bank * (-1 if points % 2 == 0 else 1)
     print('Spēle beidzās, gala punkti:', points)
     print('Uzvar ', (points % 2) + 1, '. spēlētājs', sep='')
