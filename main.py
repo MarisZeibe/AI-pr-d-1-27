116e6328f198a8b99e497a40e539094bc28784e9
MarisZeibe <maris_zeibe@hotmail.com>
2024-04-07 19:12:58 +0300

    Added limited search depth, refactoring and cleanup

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import random
 from enum import Enum
+
+GUI_MODE = False
+DEBUG = True
+SEARCH_DEPTH = 2
+END_NUMBER = 3000
+MIN_START_NUMBER = 20
+MAX_START_NUMBER = 30
+MIN_MULTIPLIER = 3
+MAX_MULTIPLIER = 5
 
 
 class Algorithm(Enum):
@@ -8,8 +16,194 @@ class Algorithm(Enum):
 
 
 class Player(Enum):
-    Lietotajs = 1
-    Dators = 2
+    USER = 1
+    COMPUTER = 2
+
+
+class State:
+    number: int
+    points: int
+    bank: int
+    level: int
+    children: list
+    value: float | None
+
+    def __init__(self, number, points, bank, level) -> None:
+        self.number = number
+        self.points = points
+        self.bank = bank
+        self.level = level
+        self.children = []
+        self.value = None
+
+    def __eq__(self, other) -> bool:
+        if other.__class__ is not self.__class__:
+            return NotImplemented
+        return ((self.number, self.points, self.bank, self.level) ==
+                (other.number, other.points, other.bank, other.level))
+
+    def next_state(self, multiplier: int) -> 'State':
+        new_state = State(
+            self.number * multiplier,
+            self.points + (1 if (self.number * multiplier) % 2 == 0 else -1),
+            self.bank + (1 if (self.number * multiplier) % 5 == 0 else 0),
+            self.level + 1
+        )
+        if new_state.number >= END_NUMBER:
+            new_state.points += new_state.bank * (-1 if new_state.points % 2 == 0 else 1)
+        return new_state
+
+    def evaluate_state(self) -> float:
+        max_number = END_NUMBER * MAX_MULTIPLIER
+        # Vai stāvoklis ir labvēlīgs pirmajam spēlētājam (maksimizētājam) vai otrajam spēlētājam (minimizētājam)
+        k = 1 if (((self.bank if self.number < END_NUMBER else 0) + self.points) % 2 == 0) else -1
+        # Vai spēles banka ir lielāka par 0
+        x1 = min(self.bank, 1)
+        # Ja ir spēles beigu stāvoklis, mazākam skaitlim ir lielāka vērtība
+        x2 = (1 if self.number >= END_NUMBER else 0) * (max_number - self.number) / (max_number - 20)
+        # Ja nav spēles beigu stāvoklis, lielākam skaitlim ir lielāka vērtība
+        x3 = (0 if self.number >= END_NUMBER else 1) * self.number / (max_number - 20)
+
+        return k * (100*x1 + 10*x2 + 1*x3)
+
+
+def generate_tree(tree: list[State], index=0, depth=SEARCH_DEPTH) -> list[State]:
+    tree[index].children = []
+    if tree[index].number >= END_NUMBER:
+        return tree
+    for multiplier in range(MIN_MULTIPLIER, MAX_MULTIPLIER + 1):
+        new_state = tree[index].next_state(multiplier)
+        if new_state not in tree:
+            tree.append(new_state)
+            tree[index].children.append(len(tree) - 1)
+            if depth > 1:
+                tree = generate_tree(tree, len(tree) - 1, depth - 1)
+        else:
+            tree[index].children.append(tree.index(new_state))
+    return tree
+
+
+def print_tree(tree: list[State], algorithm: Algorithm | None = None, index=0) -> None:
+    if algorithm == Algorithm.MINIMAX:
+        minimax_search(tree)
+    elif algorithm == Algorithm.ALPHA_BETA:
+        alpha_beta_search(tree)
+    print('\t' * tree[index].level, end='')
+    print(f'number: {tree[index].number:<5}', end=' ')
+    print(f'points: {tree[index].points:<2}', end=' ')
+    print(f'bank: {tree[index].bank}', end=' ')
+    print(f'value: {tree[index].evaluate_state():<9.4f}', end=' ')
+    if tree[index].value is not None:
+        print(f'algorithm: {tree[index].value:.4f}', end='')
+    print()
+    for state_index in tree[index].children:
+        print_tree(tree, None, state_index)
+
+
+def minimax_search(tree: list[State], index=0) -> dict[str, float]:
+    if len(tree[index].children) == 0:
+        return {'value': tree[index].evaluate_state()}
+
+    if tree[index].level % 2 == 0:
+        best = {'value': float('-inf')}
+        for child_index in tree[index].children:
+            child = {'value': minimax_search(tree, child_index)['value'],
+                     'index': tree[index].children.index(child_index)}
+            if child['value'] > best['value']:
+                best = child
+                tree[child_index].value = best['value']
+    else:
+        best = {'value': float('inf')}
+        for child_index in tree[index].children:
+            child = {'value': minimax_search(tree, child_index)['value'],
+                     'index': tree[index].children.index(child_index)}
+            if child['value'] < best['value']:
+                best = child
+                tree[child_index].value = best['value']
+    tree[index].value = best['value']
+    return best
+
+
+def alpha_beta_search(tree: list[State], index=0, alpha=float('-inf'), beta=float('inf')) -> dict[str, float]:
+    if len(tree[index].children) == 0:
+        return {'value': tree[index].evaluate_state()}
+
+    if tree[index].level % 2 == 0:
+        best = {'value': float('-inf')}
+        for child_index in tree[index].children:
+            child = {'value': alpha_beta_search(tree, child_index, alpha, beta)['value'],
+                     'index': tree[index].children.index(child_index)}
+            if child['value'] > best['value']:
+                best = child
+                tree[child_index].value = best['value']
+            alpha = max(alpha, best['value'])
+            if alpha >= beta:
+                break
+    else:
+        best = {'value': float('inf')}
+        for child_index in tree[index].children:
+            child = {'value': alpha_beta_search(tree, child_index, alpha, beta)['value'],
+                     'index': tree[index].children.index(child_index)}
+            if child['value'] < best['value']:
+                best = child
+                tree[child_index].value = best['value']
+            beta = min(beta, best['value'])
+            if alpha >= beta:
+                break
+    tree[index].value = best['value']
+    return best
+
+
+class Game:
+    state: State
+    algorithm: Algorithm
+    players: list[Player]
+
+    def __init__(self, starting_player: Player, algorithm: Algorithm, starting_number: int) -> None:
+        self.state = State(starting_number, 0, 0, 0)
+        self.algorithm = algorithm
+        if starting_player == Player.USER:
+            self.players = [Player.USER, Player.COMPUTER]
+        else:
+            self.players = [Player.COMPUTER, Player.USER]
+        if DEBUG:
+            print_tree(generate_tree([self.state], 0, 10 ** 5), algorithm)
+
+    def user_move(self, multiplier: int) -> None:
+        self.state = self.state.next_state(multiplier)
+
+    def computer_move(self) -> int:
+        tree = generate_tree([self.state])
+        if self.algorithm == Algorithm.MINIMAX:
+            multiplier = int(minimax_search(tree)['index']) + MIN_MULTIPLIER
+        else:
+            multiplier = int(alpha_beta_search(tree)['index']) + MIN_MULTIPLIER
+        self.user_move(multiplier)
+        if DEBUG:
+            print_tree(tree, self.algorithm)
+        return multiplier
+
+    def get_current_player(self) -> Player:
+        return self.players[self.state.level % 2]
+
+    def is_game_finished(self) -> bool:
+        return self.state.number >= END_NUMBER
+
+    def get_winner(self) -> Player:
+        return self.players[self.state.points % 2]
+
+
+def int_input(message: str, number_range: range) -> int:
+    print(message, end='')
+    while True:
+        try:
+            input_number = int(input())
+            if input_number in number_range:
+                return input_number
+            else:
+                print('Nepareiz skaitlis, mēģiniet vēlreiz: ', end='')
+        except ValueError:
+            print('Kļūda, mēģiniet vēlreiz: ', end='')
 
 
 def choose_starting_player() -> Player:
@@ -18,9 +212,9 @@ def choose_starting_player() -> Player:
         print("Lietotājs vai dators")
         choice = input("Ievadiet izvēlēto spēlētāju (l vai d): ")
         if choice == "l":
-            return Player.Lietotajs
+            return Player.USER
         elif choice == "d":
-            return Player.Dators
+            return Player.COMPUTER
         else:
             print("Nepareiza izvēle. Mēģiniet vēlreiz.")
 
@@ -38,190 +232,32 @@ def choose_algorithm() -> Algorithm:
             print("Nepareiza izvēle. Mēģiniet vēlreiz.")
 
 
-def evaluate_state(state):
-    # Vai stāvoklis ir labvēlīgs pirmajam spēlētājam (maksimizētājam) vai otrajam spēlētājam (minimizētājam)
-    k = 1 if ((state.bank + state.points) % 2 == 0) else -1
-    # Vai spēles banka ir lielāka par 0
-    x1 = min(state.bank, 1)
-    # Ja ir spēles beigu stāvoklis, mazākam skaitlim ir lielāka vērtība
-    x2 = (1 if state.number >= 3000 else 0) * (3000 * 5 - state.number) / (3000 * 5 - 20)
-    # Ja nav spēles beigu stāvoklis, lielākam skaitlim ir lielāka vērtība
-    x3 = (0 if state.number >= 3000 else 1) * state.number / (3000 * 5 - 20)
-
-    return k * (100*x1 + 10*x2 + 1*x3)
+def choose_starting_number() -> int:
+    return int_input(f'Ievadiet skaitli no {MIN_START_NUMBER} līdz {MAX_START_NUMBER}: ',
+                     range(MIN_START_NUMBER, MAX_START_NUMBER + 1))
 
 
-def minimax_search(state, player):
-    if state.number >= 3000:
-        return evaluate_state(state)
-
-    if player == Player.Lietotajs:
-        best_value = float('-inf')
-        for child_index in state.children:
-            child_value = minimax_search(gameTree[child_index], Player.Dators)
-            best_value = max(best_value, child_value)
-        return best_value
-    else:
-        best_value = float('inf')
-        for child_index in state.children:
-            child_value = minimax_search(gameTree[child_index], Player.Lietotajs)
-            best_value = min(best_value, child_value)
-        return best_value
-
-
-def alpha_beta_search(state, alpha, beta, player):
-    if state.number >= 3000:
-        return evaluate_state(state)
-
-    if player == Player.Lietotajs:
-        value = float('-inf')
-        for child_index in state.children:
-            child_value = alpha_beta_search(gameTree[child_index], alpha, beta, Player.Dators)
-            value = max(value, child_value)
-            alpha = max(alpha, value)
-            if alpha >= beta:
-                break
-        return value
-    else:
-        value = float('inf')
-        for child_index in state.children:
-            child_value = alpha_beta_search(gameTree[child_index], alpha, beta, Player.Lietotajs)
-            value = min(value, child_value)
-            beta = min(beta, value)
-            if alpha >= beta:
-                break
-        return value
-
-
-class State:
-    number: int
-    points: int
-    bank: int
-    level: int
-    children: list
-
-    def __init__(self, number, points, bank, level):
-        self.number = number
-        self.points = points
-        self.bank = bank
-        self.level = level
-        self.children = []
-
-    def __eq__(self, other):
-        if other.__class__ is not self.__class__:
-            return NotImplemented
-        return (self.number, self.points, self.bank, self.level) == (other.number, other.points, other.bank, other.level)
-
-
-def int_input(message: str, number_range: range) -> int:
-    print(message, end='')
+if GUI_MODE:
+    pass
+else:
     while True:
-        try:
-            input_number = int(input())
-            if input_number in number_range:
-                return input_number
+        game = Game(choose_starting_player(), choose_algorithm(), choose_starting_number())
+    
+        while not game.is_game_finished():
+            if game.get_current_player() == Player.USER:
+                print('Lietotāja gājiens:')
+                input_multiplier = int_input(f'Ievadiet reizinātāju no {MIN_MULTIPLIER} līdz {MAX_MULTIPLIER}: ',
+                                             range(MIN_MULTIPLIER, MAX_MULTIPLIER + 1))
+                game.user_move(input_multiplier)
             else:
-                print('Nepareiz skaitlis, mēģiniet vēlreiz: ', end='')
-        except ValueError:
-            print('Kļūda, mēģiniet vēlreiz: ', end='')
-
-
-def generate_tree(index=0):
-    global gameTree
-    if gameTree[index].number > 3000:
-        return
-    for n in range(3, 6):
-        new_state = State(
-            gameTree[index].number * n,
-            gameTree[index].points + (1 if (gameTree[index].number*n) % 2 == 0 else -1),
-            gameTree[index].bank + (1 if (gameTree[index].number*n) % 5 == 0 else 0),
-            gameTree[index].level + 1
-        )
-        if new_state.number >= 3000:
-            new_state.points += bank * (-1 if new_state.points % 2 == 0 else 1)
-        if new_state not in gameTree:
-            gameTree.append(new_state)
-            gameTree[index].children.append(len(gameTree) - 1)
-            generate_tree(len(gameTree) - 1)
-        else:
-            gameTree[index].children.append(gameTree.index(new_state))
-
-
-def print_tree(generate_values=False, index=0):
-    print('\t' * gameTree[index].level, end='')
-    print(f'number: {gameTree[index].number:<5}', end=' ')
-    print(f'points: {gameTree[index].points:<2}', end=' ')
-    print(f'bank: {gameTree[index].bank}', end=' ')
-    if generate_values:
-        print(f'value: {evaluate_state(gameTree[index]):<9.4f}', end=' ')
-        current_player = player.Lietotajs if gameTree[index].level % 2 == 0 else player.Dators
-        print(f'minimax: {minimax_search(gameTree[index], current_player):.4f}', end='')
-    print()
-    for state_index in gameTree[index].children:
-        print_tree(generate_values, state_index)
-
-
-while True:
-    points = 0
-    bank = 0
-    player = choose_starting_player()
-    algorithm = choose_algorithm()
-    number = int_input('Ievadiet skaitli no 20 līdz 30: ', range(20, 31))
-
-    gameTree = [State(number, points, bank, 0)]
-    generate_tree()
-    print_tree(True)
-
-    while number <= 3000:
-        print(player, '. spēlētāja gājiens', sep='')
-        if player == Player.Lietotajs:
-            number *= int_input('Ievadiet reizinātāju (3, 4 vai 5): ', range(3, 6))
-        else:
-            if algorithm == Algorithm.MINIMAX:
-                available_moves = [3, 4, 5]
-                best_value = float('-inf')
-                best_move = None
-                for move in available_moves:
-                    new_number = number * move
-                    new_points = points + (1 if new_number % 2 == 0 else -1)
-                    new_bank = bank + (1 if new_number % 5 == 0 else 0)
-                    new_level = gameTree[0].level + 1
-                    child_state = State(new_number, new_points, new_bank, new_level)
-                    value = minimax_search(child_state, player)
-                    if value > best_value:
-                        best_value = value
-                        best_move = move
-
-                number *= best_move
-                print('Datora gājiens. Izvēlēts skaitlis:', best_move)
-            elif algorithm == Algorithm.ALPHA_BETA:
-                available_moves = [3, 4, 5]
-                best_value = float('-inf')
-                best_move = None
-                for move in available_moves:
-                    new_number = number * move
-                    new_points = points + (1 if new_number % 2 == 0 else -1)
-                    new_bank = bank + (1 if new_number % 5 == 0 else 0)
-                    new_level = gameTree[0].level + 1
-                    child_state = State(new_number, new_points, new_bank, new_level)
-                    value = alpha_beta_search(child_state, float('-inf'), float('inf'), player)
-                    if value > best_value:
-                        best_value = value
-                        best_move = move
-
-                number *= best_move
-                print('Datora gājiens. Izvēlēts skaitlis:', best_move)
-
-        points += 1 if number % 2 == 0 else -1
-        bank += 1 if number % 5 == 0 else 0
-        # player = (player % 2) + 1
-        player = Player.Lietotajs if player == Player.Dators else Player.Dators
-        print('skailtis:', number, '| punkti:', points, '| banka:', bank)
-
-    points += bank * (-1 if points % 2 == 0 else 1)
-    print('Spēle beidzās, gala punkti:', points)
-    print('Uzvar ', (points % 2) + 1, '. spēlētājs', sep='')
-
-    play_again = input("Vai vēlaties spēlēt vēlreiz? (j/n): ")
-    if play_again.lower() != "j":
-        break
+                print('Datora gājiens:')
+                print('Dators izvēlējās skaitli:', game.computer_move())
+    
+            print('skailtis:', game.state.number, '| punkti:', game.state.points, '| banka:', game.state.bank)
+    
+        print('Spēle beidzās, gala punkti:', game.state.points)
+        print('Uzvarēja', 'lietotājs' if game.get_winner() == Player.USER else 'dators')
+    
+        play_again = input("Vai vēlaties spēlēt vēlreiz? (j/n): ")
+        if play_again.lower() != "j":
+            break
